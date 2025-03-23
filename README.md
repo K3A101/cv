b37f51f5b3162d6bb57bccb908609477f68928da
K3A101 <alexanderkeisha9@gmail.com>
2025-03-23 11:20:36 +0100

    Initial setup

---
+++ README.md
@@ -0,0 +1,8 @@
+# Mijn CV
+Dit is mijn digitale cv.
+
+## Talen
+- [ ] HTML
+- [ ] CSS
+- [ ] Javascript
+
---
+++ static/js/swiper.js
@@ -0,0 +1,25 @@
+const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    loop: true,
+    autoplay: true,
+    autoplay: {
+        delay: 5000,
+    },
+    // autoHeight: true,
+    centeredSlides: true,
+    grabCursor: true,
+    spaceBetween: 15,
+    slidesPerView: 1.5,
+
+
+    //Use arrow keys to navigate slider
+    keyboard: {
+        enabled: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev"
+    }
+});
